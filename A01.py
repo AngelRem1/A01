d146e1ef70154450d3ef797e95886228e345260c
AngelRem1 <51797591+AngelRem1@users.noreply.github.com>
2020-09-12 17:27:20 -0700

    Add files via upload

---
+++ A01.py
@@ -0,0 +1,12 @@
+#
+# Name : Angel Remigio
+# SSID : 1848012
+# Assignment #: 1
+# Submission Date : 9/12/2020
+#
+# Description of program :
+# This program lists five places I recommend (or want ) to visit in the world
+#
+places = ['Copper Canyon', 'Jalisco', 'Tokyo', 'Paris', 'Stonehenge']
+for idx, val in enumerate(places):
+    print("%d An example of a place I want to visit is %s" % (idx, val))
